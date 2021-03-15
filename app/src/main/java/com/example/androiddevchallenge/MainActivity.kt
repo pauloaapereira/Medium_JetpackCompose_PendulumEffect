06e94f884cbbccf368c09da18573fc23cc78d85a
Paulo Pereira <paulo_aa_pereira@outlook.pt>
2021-03-15 15:42:13 +0000

    # Added pendulum example

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,11 +18,26 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -39,23 +54,62 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+
+        Pendulum {
+            Surface(
+                modifier = Modifier.size(24.dp),
+                shape = CircleShape,
+                color = Color.Blue,
+                content = {})
+        }
+
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
-    }
-}
+fun Pendulum(
+    modifier: Modifier = Modifier,
+    swingDuration: Int = 1500,
+    startX: Float = .2f,
+    endX: Float = .8f,
+    topY: Float = .2f,
+    bottomY: Float = .4f,
+    content: @Composable () -> Unit
+) {
+    val infiniteTransition = rememberInfiniteTransition()
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+    BoxWithConstraints(
+        modifier = modifier
+    ) {
+
+        val start = maxWidth * startX
+        val end = maxWidth * endX
+        val top = maxHeight * topY
+        val bottom = maxHeight * bottomY
+
+        val x by infiniteTransition.animateFloat(
+            initialValue = start.value,
+            targetValue = end.value,
+            animationSpec = infiniteRepeatable(
+                animation = tween(swingDuration, easing = LinearEasing),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
+        val y by infiniteTransition.animateFloat(
+            initialValue = top.value,
+            targetValue = bottom.value,
+            animationSpec = infiniteRepeatable(
+                animation = tween(swingDuration / 2, easing = LinearOutSlowInEasing),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
+        Box(
+            modifier = Modifier.offset(x = x.dp, y = y.dp)
+        ) {
+            content()
+        }
     }
 }
